dec004e935ffba366de16d428bf75bcad961812c
Salvatore <salvatore@rezo.net>
2022-06-09 01:27:01 +0200

    [Salvatore] [source:lang/ urls] Export depuis https://trad.spip.net de la langue fr [Salvatore] [source:lang/ urls] Mise a jour du bilan depuis https://trad.spip.net

--- lang/urls_fr.php
+++ lang/urls_fr.php
@@ -32,8 +32,8 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	// L
 	'label_tri_date' => 'Date',
 	'label_tri_id' => 'ID',
-	'label_tri_url' => 'URL',
 	'label_tri_perma' => 'URLs verrouillées',
+	'label_tri_url' => 'URL',
 	'label_url' => 'Nouvelle URL',
 	'label_url_minuscules_0' => 'Conserver la casse du titre',
 	'label_url_minuscules_1' => 'Forcer les URLs en minuscules',
